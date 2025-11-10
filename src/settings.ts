9496c6206fc5eb8e37b11c675f1b2b5f0262e540
Matthieu Di Berardino <yenoiwesa@gmail.com>
2025-11-10 19:51:45 +1100

    Correct PLATFORM_NAME casing for consistency

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'DaikinAirBase';
+export const PLATFORM_NAME = 'DaikinAirbase';
 
 /**
  * This must match the name of your plugin as defined the package.json `name` property
